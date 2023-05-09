c78becd2c6cd1c5af82ee8d19eb09c4d4cb4eb77
Cecilia Marçal <116651825+ceferrei@users.noreply.github.com>
2023-05-09 10:05:26 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using Banco;
+using System.ComponentModel;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Account acc = new Account();
+        Console.Write("Enter account number: ");
+        int accountNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter account holder: ");
+        string accountHolder = Console.ReadLine();
+        Console.Write("Is there an initial deposit (y/n)? ");
+        char response = char.Parse(Console.ReadLine());
+        if (response == 'y' || response == 'Y')
+        {
+            Console.Write("Enter initial deposit value: ");
+            double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            acc = new Account(accountNumber, accountHolder, initialDeposit);
+        }
+        else
+        {
+            acc = new Account(accountNumber, accountHolder); // assuming zero balance
+        }
+
+        Console.WriteLine("Account data:\n" + acc);
+
+        Console.Write("Enter a deposit value: ");
+        int depositValue = int.Parse(Console.ReadLine());
+        acc.Deposit(depositValue);
+        Console.WriteLine("Updated account data:\n" + acc);
+
+        Console.Write("Enter a withdrawal value: ");
+        depositValue = int.Parse(Console.ReadLine());
+        acc.Withdraw(depositValue);
+        Console.WriteLine("Updated account data:\n" + acc);
+
+        acc.AccountHolder = "Maria";
+        Console.WriteLine(acc); // changed account holder name to Maria
+    }
+}
+
+
+
+namespace Banco
+{
+    internal class Account
+    {
+        // Properties
+        public int AccountNumber { get; } // we don't use the set because it doesn't make sense to change the account number
+        public string AccountHolder { get; set; } // can be changed, e.g. after marriage
+        public double Balance { get; private set; } // public to get the value, but private to modify the balance
+        public static double WithdrawalFee = 5;
+        public double InitialDeposit;
+
+        // Constructors - always public and use the name of the class
+        public Account() { }
+
+        // This constructor allows instantiating accounts with just the account number and account holder
+        public Account(int accountNumber, string accountHolder)
+        {
+            AccountHolder = accountHolder;
+            AccountNumber = accountNumber;
+        }
+
+        // This constructor allows instantiating accounts with the account number, account holder, and initial balance
+        public Account(int accountNumber, string accountHolder, double balance) : this(accountNumber, accountHolder) // using "this" to reuse code from the other constructor
+        {
+            Balance = balance;
+        }
+
+        // Methods
+        public void Withdraw(double withdrawal)
+        {
+            Balance -= withdrawal + WithdrawalFee;
+        }
+        public void Deposit(double deposit)
+        {
+            Balance += deposit;
+        }
+
+        public override string ToString()
+        {
+            return
+                "Account: "
+                + AccountNumber
+                + ", Account Holder: "
+                + AccountHolder
+                + ", Balance: "
+                + Balance.ToString("F2", CultureInfo.InvariantCulture)
+                + "euros.";
+        }
+    }
+}
