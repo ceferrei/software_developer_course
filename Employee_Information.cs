01446fb99d7c2bd75825ddd087a38bdb71b53817
Cecilia Marçal <116651825+ceferrei@users.noreply.github.com>
2023-05-12 10:36:58 +0100

    Update Employee_Information.cs

--- Employee_Information.cs
+++ Employee_Information.cs
@@ -1,10 +1,6 @@
 ﻿using Employee_Information;
 using System.Globalization;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 internal class Program
 {
