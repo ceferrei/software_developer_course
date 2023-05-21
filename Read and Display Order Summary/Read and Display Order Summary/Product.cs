c7e35cf7ff88b634247ea6b4cdc5e5fbc8c6c7e1
Cecilia Marçal <116651825+ceferrei@users.noreply.github.com>
2023-05-21 17:19:07 +0100

    Add files via upload

---
+++ Read and Display Order Summary/Read and Display Order Summary/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read_and_Display_Order_Summary
+{
+    internal class Product
+    {
+        public string NameProduct { get; set; }
+        public double PriceProduct { get; set; }
+
+        public Product() { }
+
+        public Product(string nameProduct, double priceProduct)
+        {
+            NameProduct = nameProduct;
+            PriceProduct = priceProduct;
+        }
+    }
+}
