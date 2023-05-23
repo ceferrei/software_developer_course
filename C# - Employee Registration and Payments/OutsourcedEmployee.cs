3acc838d3cf8a83b777b6e29c562992abddc2bb6
Cecilia Marçal <116651825+ceferrei@users.noreply.github.com>
2023-05-23 15:12:28 +0100

    Add files via upload

---
+++ C# - Employee Registration and Payments/OutsourcedEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Registration_and_Payments
+{
+    internal class OutsourcedEmployee: Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+            
+        }
+
+        public OutsourcedEmployee(double additionalCharge, string name, int hours, double valuePerHour) 
+            :base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+        public override double Payment()
+        {
+            return base.Payment() + AdditionalCharge * 1.1;
+        }
+
+
+    }
+}
---
+++ C# - Employee Registration and Payments/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace Employee_Registration_and_Payments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of employees: ");
+            int numberE = int.Parse(Console.ReadLine());
+
+            List<Employee> employees = new List<Employee>();
+
+            for(int i = 1; i <= numberE; i++) 
+            {
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.Write("Outsourced (y/n): ");
+                string yesOrNo = Console.ReadLine();
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value por hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+
+                if(yesOrNo == "y" || yesOrNo == "Y")
+                {
+                    Console.Write("Additional charge: ");
+                    double addCharge = double.Parse(Console.ReadLine());
+                    employees.Add(new OutsourcedEmployee(addCharge, name, hours, valuePerHour));
+                }
+                else if(yesOrNo == "n" || yesOrNo== "N") 
+                {
+                    employees.Add(new Employee( name, hours, valuePerHour));
+                }
+                else
+                {
+                    Console.WriteLine("Error, the program will close.");
+                    return;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach(Employee employee in employees)
+            {
+                Console.WriteLine($"{employee.Name} - $ {employee.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
