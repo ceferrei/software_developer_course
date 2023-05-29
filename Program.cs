fde58b89c2654335ca57c60ba6eeb83b50c59d29
Cecilia Marçal <116651825+ceferrei@users.noreply.github.com>
2023-05-29 16:58:16 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,51 +2,56 @@
 using System.Collections.Generic;
 
 namespace V01
-{ 
+{
+    // Classe que representa um pedido
     public class Order
     {
-        public int OrderId { get; set; }
-        public string CustomerName { get; set; }
-        public List<string> Items { get; set; }
-        public OrderStatus Status { get; set; }
+        public int OrderId { get; set; } // ID do pedido
+        public string CustomerName { get; set; } // Nome do cliente
+        public List<string> Items { get; set; } // Itens do pedido
+        public OrderStatus Status { get; set; } // Status do pedido
     }
 
+    // Enumeração que representa o status do pedido
     public enum OrderStatus
     {
-        Pending,
-        Preparing,
-        Served
+        Pending, // Pendente
+        Preparing, // Preparando
+        Served // Servido
     }
 
+    // Classe responsável por gerenciar os pedidos
     public class OrderManager
     {
-        private List<Order> orders;
+        private List<Order> orders; // Lista de pedidos
 
         public OrderManager()
         {
-            orders = new List<Order>();
+            orders = new List<Order>(); // Inicializa a lista de pedidos
         }
 
+        // Método para realizar um novo pedido
         public void PlaceOrder(string customerName, List<string> items)
         {
             var newOrder = new Order
             {
-                OrderId = orders.Count + 1,
+                OrderId = orders.Count + 1, // Atribui um novo ID para o pedido
                 CustomerName = customerName,
                 Items = items,
-                Status = OrderStatus.Pending
+                Status = OrderStatus.Pending // Define o status inicial como Pendente
             };
 
-            orders.Add(newOrder);
+            orders.Add(newOrder); // Adiciona o pedido à lista de pedidos
             Console.WriteLine($"Order {newOrder.OrderId} placed for {customerName}");
         }
 
+        // Método para atualizar o status de um pedido existente
         public void UpdateOrderStatus(int orderId, OrderStatus status)
         {
-            var order = orders.Find(o => o.OrderId == orderId);
+            var order = orders.Find(o => o.OrderId == orderId); // Procura o pedido pelo ID
             if (order != null)
             {
-                order.Status = status;
+                order.Status = status; // Atualiza o status do pedido
                 Console.WriteLine($"Order {order.OrderId} status updated to {status}");
             }
             else
@@ -55,9 +60,10 @@ namespace V01
             }
         }
 
+        // Método para exibir os detalhes de um pedido
         public void DisplayOrderDetails(int orderId)
         {
-            var order = orders.Find(o => o.OrderId == orderId);
+            var order = orders.Find(o => o.OrderId == orderId); // Procura o pedido pelo ID
             if (order != null)
             {
                 Console.WriteLine($"Order ID: {order.OrderId}");
@@ -82,19 +88,18 @@ namespace V01
         {
             var orderManager = new OrderManager();
 
-            // Place an order
+            // Realiza um pedido
             orderManager.PlaceOrder("John Doe", new List<string> { "Burger", "Fries", "Coke" });
 
-            // Place another order
+            // Realiza outro pedido
             orderManager.PlaceOrder("Jane Smith", new List<string> { "Pizza", "Salad", "Water" });
 
-            // Update order status
+            // Atualiza o status de um pedido
             orderManager.UpdateOrderStatus(1, OrderStatus.Preparing);
 
-            // Display order details
+            // Exibe os detalhes do pedido
             orderManager.DisplayOrderDetails(1);
             orderManager.DisplayOrderDetails(2);
         }
     }
-
 }
